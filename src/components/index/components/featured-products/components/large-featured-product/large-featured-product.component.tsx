5c9636e7776c6f2d0b5bae655fa44b0f1a33fde0
stelko <stilyanmtoskov@gmail.com>
2023-04-27 13:57:29 +0100

    features tablet styling

--- src/components/index/components/featured-products/components/large-featured-product/large-featured-product.component.tsx
+++ src/components/index/components/featured-products/components/large-featured-product/large-featured-product.component.tsx
@@ -1,31 +1,36 @@
-import Button from '@/components/global/button/button.component';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import ZX9Speaker from 'public/assets/home/mobile/image-speaker-zx9.png';
-import Circles from 'public/assets/home/desktop/pattern-circles.svg';
-import Link from 'next/link';
+
+import Button from '@/components/global/button/button.component';
 
 export default function LargeFeaturedProduct() {
   return (
-    <div className="flex flex-col items-center gap-8 overflow-hidden rounded-lg bg-orange-700 px-6 pt-14 text-center text-white">
-      <div className="relative">
-        <Image
-          src={ZX9Speaker}
-          alt="ZX9 Speaker"
-          className="relative z-10 w-[172px]"
-        />
-        <Image
-          src={Circles}
-          alt=""
-          className="absolute -left-48 -top-44 min-w-[558px]"
-        />
+    <div className="relative flex flex-col items-center gap-8 overflow-hidden rounded-lg bg-orange-700 px-6 pt-14 text-center text-white md:gap-16">
+      <div className="before:absolute before:-bottom-8 before:left-1/2 before:-translate-x-1/2 before:scale-[59%] before:content-[url('/assets/home/desktop/pattern-circles.svg')] md:before:bottom-12 md:before:scale-100">
+        <picture className="relative z-10">
+          <source
+            media="(min-width:768px)"
+            srcSet="assets/home/tablet/image-speaker-zx9.png"
+          />
+          <Image
+            src={ZX9Speaker}
+            alt="ZX9 Speaker"
+            className="w-[172px] md:w-[195px]"
+          />
+        </picture>
       </div>
-      <div className="relative flex flex-col items-center gap-6 pb-14">
-        <h3 className="heading-largest px-3">ZX9 Speaker</h3>
-        <p className="opacity-75">
-          Upgrade to premium speakers that are phenomenally built to deliver
-          truly remarkable sound.
-        </p>
+      <div className="relative flex flex-col items-center gap-6 pb-14 md:w-[349px] md:gap-10">
+        <div className="flex flex-col items-center gap-6">
+          <h3 className="heading-largest md:heading-largest-tablet px-3">
+            ZX9 Speaker
+          </h3>
+          <p className="opacity-75">
+            Upgrade to premium speakers that are phenomenally built to deliver
+            truly remarkable sound.
+          </p>
+        </div>
         <Link href="/product-detail/xz9-speaker">
           <Button type="secondary">
             <span>See Product</span>
